a875d7790be91aed3a5f619a5a30b09dca908c7d
Christopher-Deo <chris@christopherdeo.com>
2023-10-10 06:49:44 -0500

    completed hangman game exercise

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ row2 = ["⬜️", "⬜️", "️⬜️"]
 row3 = ["⬜️️", "⬜️️", "⬜️️"]
 map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
-position = input("Where do you want to put the treasure? ")
+position = eval(input("Where do you want to put the treasure? "))
 # assigning user's choice to variable to be able select the position on the map
 selected_column = int(position[0])
 selected_row = int(position[1])
